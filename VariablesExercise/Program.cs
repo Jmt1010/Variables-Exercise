9bf827053035cfe4b24697c765348d874ca4b541
Jared Thompson <jaredt10@gmail.com>
2024-02-12 14:36:15 -0600

    completed variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string name;
+int age;
+char middleInital;
+bool isOver18;
+double currentTemp;
+decimal currentPrice;
+
+name = "Jared";
+age = 30;
+middleInital = 'M';
+isOver18 = true;
+currentTemp = 60.0;
+currentPrice = 30.00m;
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Inital: {middleInital}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
